dd07d7ee0817b909c454f8fe5a9cb3fb9d76c46d
nishantshah <nishantshah1729@gmail.com>
2021-10-13 16:24:10 +0530

    doen

--- Hello.py
+++ Hello.py
@@ -1,8 +1,2 @@
-import os
-a = 5
-# print(type(2 > 3))
-boll1 = True
-boll2 = False
-print("The value of & is ", (boll1 and boll2))
-print("The value of & is ", (boll1 or boll2))
-print("The value of & is ", (not boll2))
+a = int(input("Enter a no. "))
+print(a % 2)
