1a87e1223a75ddc22113c045c50baf6fd35683a3
nishantshah <nishantshah1729@gmail.com>
2021-10-13 19:09:37 +0530

    doen

--- Hello.py
+++ Hello.py
@@ -1,3 +1,14 @@
-a=(1,)
+{# a=input("Enter a word to check its meaning\n:")
+# b=input("Enter a word to check its meaning\n:")
+# c=input("Enter a word to check its meaning\n:")
+# d=input("Enter a word to check its meaning\n:")
+# e=input("Enter a word to check its meaning\n:")
+# s={1,1.0}
+# # s.add(a)
+# # s.add(b)
+# # s.add(c)
+# # s.add(d)
+# # s.add(e)
+# print(s)
+}
 
-print(a)
