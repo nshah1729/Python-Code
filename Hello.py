f9f2628e3595fc9903e01878d2af9a0dc9a109ec
nishantshah <nishantshah1729@gmail.com>
2021-10-13 15:57:35 +0530

    Python Done

---
+++ Hello.py
@@ -0,0 +1,8 @@
+import os
+a = 5
+# print(type(2 > 3))
+boll1 = True
+boll2 = False
+print("The value of & is ", (boll1 and boll2))
+print("The value of & is ", (boll1 or boll2))
+print("The value of & is ", (not boll2))
