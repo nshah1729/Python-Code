bde51e2d6b05080105c5baf1a35f09a1eb9ac558
nishantshah <nishantshah1729@gmail.com>
2021-10-13 20:38:55 +0530

    doneefe

--- Hello.py
+++ Hello.py
@@ -1,14 +1,7 @@
-{# a=input("Enter a word to check its meaning\n:")
-# b=input("Enter a word to check its meaning\n:")
-# c=input("Enter a word to check its meaning\n:")
-# d=input("Enter a word to check its meaning\n:")
-# e=input("Enter a word to check its meaning\n:")
-# s={1,1.0}
-# # s.add(a)
-# # s.add(b)
-# # s.add(c)
-# # s.add(d)
-# # s.add(e)
-# print(s)
-}
+a = 4
+if(a > 5):
+    print("Chunumunu")
+else:
+    print("bsjdbfs")
+
 
